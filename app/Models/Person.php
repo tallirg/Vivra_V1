8888af0b416c59bae8f4a265332e61e04a352e27
Francisco <fppj0987@gmail.com>
2026-07-09 20:30:16 +0000

    CORS

--- app/Models/Person.php
+++ app/Models/Person.php
@@ -2,9 +2,19 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Model;
+// CAMBIAMOS LA IMPORTACIÓN AQUÍ:
+// Usamos el modelo de MongoDB en lugar del de Illuminate
+use MongoDB\Laravel\Eloquent\Model;
 
 class Person extends Model
 {
-    //
+    // Nombre de la colección en MongoDB
+    protected $collection = 'people';
+
+    protected $fillable = [
+        'name',
+        'email',
+        'password',
+        'rol'
+    ];
 }
